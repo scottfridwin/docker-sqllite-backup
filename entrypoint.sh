31fe0b0483ac462a975374a5842cb7dc37d3c58e
Scott <96879441+scottfridwin@users.noreply.github.com>
2025-09-02 20:57:19 +0000

    Trying to fix permissions again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 : "${CRON_SCHEDULE:=0 2 * * *}"  # default schedule
 
 CRON_FILE=/tmp/cronjob
-mkdir -p /var/log /health
 
 # Write cron job to a file
 echo "$CRON_SCHEDULE BACKUP_SRC=$BACKUP_SRC BACKUP_DEST=$BACKUP_DEST RETENTION=$RETENTION /backup.sh >> /var/log/backup.log 2>&1" > $CRON_FILE
